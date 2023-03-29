outline/outline

commit 05a8e45f015122090ab0016065e82bddfdb80b8d

Feat: zoom selected image on pressing space key (#5059)

shared/editor/nodes/Image.tsx
@@ -1,7 +1,7 @@
 import Token from "markdown-it/lib/token";
 import { InputRule } from "prosemirror-inputrules";
 import { Node as ProsemirrorNode, NodeSpec, NodeType } from "prosemirror-model";
-import { NodeSelection, EditorState } from "prosemirror-state";
+import { NodeSelection, EditorState, Plugin } from "prosemirror-state";
 import * as React from "react";
 import { sanitizeUrl } from "../../utils/urls";
 import { default as ImageComponent, Caption } from "../components/Image";
@@ -157,6 +157,35 @@ export default class Image extends SimpleImage {
         ];
       },
     };
+  }
+
+  get plugins() {
+    return [
+      new Plugin({
+        props: {
+          handleKeyDown: (view, event) => {
+            // prevent prosemirror's default spacebar behavior
+            // & zoom in if the selected node is image
+            if (event.key === " ") {
+              const { state } = view;
+              const { selection } = state;
+              if (selection instanceof NodeSelection) {
+                const { node } = selection;
+                if (node.type.name === "image") {
+                  const image = document.querySelector(
+                    ".ProseMirror-selectednode img"
+                  ) as HTMLImageElement;
+                  image.click();
+                  return true;
+                }
+              }
+            }
+
+            return false;
+          },
+        },
+      }),
+    ];
   }
 
   handleChangeSize = ({

vaderyang/baozi

commit 14a46f99a49eb81c3b0b8c1c0d3f3b903ebc39f5

fix: Regression in 05a8e45f015122090ab0016065e82bddfdb80b8d overrides image upload behavior

shared/editor/nodes/Image.tsx
@@ -161,6 +161,7 @@ export default class Image extends SimpleImage {
 
   get plugins() {
     return [
+      ...super.plugins,
       new Plugin({
         props: {
           handleKeyDown: (view, event) => {