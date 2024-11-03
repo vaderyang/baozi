outline/outline

commit 5780959e93d23568e7b83bd2472568e6895926ff

notificationUpdater

server/commands/notificationUpdater.ts
@@ -1,6 +1,6 @@
 import isUndefined from "lodash/isUndefined";
-import { Transaction } from "sequelize";
 import { Event, Notification } from "@server/models";
+import { APIContext } from "@server/types";
 
 type Props = {
   /** Notification to be updated */
@@ -9,25 +9,21 @@ type Props = {
   viewedAt?: Date | null;
   /** Time at which notification was archived */
   archivedAt?: Date | null;
-  /** The IP address of the user updating the notification */
-  ip: string;
-  /** The database transaction to run within */
-  transaction: Transaction;
 };
 
 /**
  * This command updates notification properties.
  *
+ * @param ctx The originating request context
  * @param Props The properties of the notification to update
  * @returns Notification The updated notification
  */
-export default async function notificationUpdater({
-  notification,
-  viewedAt,
-  archivedAt,
-  ip,
-  transaction,
-}: Props): Promise<Notification> {
+export default async function notificationUpdater(
+  ctx: APIContext,
+  { notification, viewedAt, archivedAt }: Props
+): Promise<Notification> {
+  const { transaction } = ctx.state;
+
   if (!isUndefined(viewedAt)) {
     notification.viewedAt = viewedAt;
   }
@@ -38,18 +34,12 @@ export default async function notificationUpdater({
   if (changed) {
     await notification.save({ transaction });
 
-    await Event.create(
-      {
-        name: "notifications.update",
-        userId: notification.userId,
-        modelId: notification.id,
-        teamId: notification.teamId,
-        documentId: notification.documentId,
-        actorId: notification.actorId,
-        ip,
-      },
-      { transaction }
-    );
+    await Event.createFromContext(ctx, {
+      name: "notifications.update",
+      userId: notification.userId,
+      modelId: notification.id,
+      documentId: notification.documentId,
+    });
   }
 
   return notification;

server/commands/notificationUpdater.test.ts
@@ -1,17 +1,15 @@
 import { NotificationEventType } from "@shared/types";
 import { Event } from "@server/models";
-import { sequelize } from "@server/storage/database";
 import {
   buildUser,
   buildNotification,
   buildDocument,
   buildCollection,
 } from "@server/test/factories";
+import { withAPIContext } from "@server/test/support";
 import notificationUpdater from "./notificationUpdater";
 
 describe("notificationUpdater", () => {
-  const ip = "127.0.0.1";
-
   it("should mark the notification as viewed", async () => {
     const user = await buildUser();
     const actor = await buildUser({
@@ -38,12 +36,10 @@ describe("notificationUpdater", () => {
     expect(notification.archivedAt).toBe(null);
     expect(notification.viewedAt).toBe(null);
 
-    await sequelize.transaction(async (transaction) =>
-      notificationUpdater({
+    await withAPIContext(user, (ctx) =>
+      notificationUpdater(ctx, {
         notification,
         viewedAt: new Date(),
-        ip,
-        transaction,
       })
     );
     const event = await Event.findLatest({
@@ -83,12 +79,10 @@ describe("notificationUpdater", () => {
     expect(notification.archivedAt).toBe(null);
     expect(notification.viewedAt).not.toBe(null);
 
-    await sequelize.transaction(async (transaction) =>
-      notificationUpdater({
+    await withAPIContext(user, (ctx) =>
+      notificationUpdater(ctx, {
         notification,
         viewedAt: null,
-        ip,
-        transaction,
       })
     );
     const event = await Event.findLatest({
@@ -127,12 +121,10 @@ describe("notificationUpdater", () => {
     expect(notification.archivedAt).toBe(null);
     expect(notification.viewedAt).toBe(null);
 
-    await sequelize.transaction(async (transaction) =>
-      notificationUpdater({
+    await withAPIContext(user, (ctx) =>
+      notificationUpdater(ctx, {
         notification,
         archivedAt: new Date(),
-        ip,
-        transaction,
       })
     );
     const event = await Event.findLatest({
@@ -172,12 +164,10 @@ describe("notificationUpdater", () => {
     expect(notification.archivedAt).not.toBe(null);
     expect(notification.viewedAt).toBe(null);
 
-    await sequelize.transaction(async (transaction) =>
-      notificationUpdater({
+    await withAPIContext(user, (ctx) =>
+      notificationUpdater(ctx, {
         notification,
         archivedAt: null,
-        ip,
-        transaction,
       })
     );
     const event = await Event.findLatest({

server/routes/api/notifications/notifications.ts
@@ -141,11 +141,9 @@ router.get(
     }
 
     if (!notification.viewedAt) {
-      await notificationUpdater({
+      await notificationUpdater(ctx, {
         notification,
         viewedAt: new Date(),
-        ip: ctx.request.ip,
-        transaction: ctx.state.transaction,
       });
     }
 
@@ -166,12 +164,10 @@ router.post(
     const notification = await Notification.findByPk(id);
     authorize(user, "update", notification);
 
-    await notificationUpdater({
+    await notificationUpdater(ctx, {
       notification,
       viewedAt,
       archivedAt,
-      ip: ctx.request.ip,
-      transaction: ctx.state.transaction,
     });
 
     ctx.body = {

server/test/support.ts
@@ -1,9 +1,12 @@
 import { faker } from "@faker-js/faker";
+import { Transaction } from "sequelize";
 import sharedEnv from "@shared/env";
 import env from "@server/env";
+import { User } from "@server/models";
 import onerror from "@server/onerror";
 import webService from "@server/services/web";
 import { sequelize } from "@server/storage/database";
+import { APIContext, AuthenticationType } from "@server/types";
 import TestServer from "./TestServer";
 
 export function getTestServer() {
@@ -26,4 +29,26 @@ export function getTestServer() {
  */
 export function setSelfHosted() {
   env.URL = sharedEnv.URL = `https://${faker.internet.domainName()}`;
+}
+
+export function withAPIContext(
+  user: User,
+  fn: (ctx: APIContext) => unknown
+): Promise<unknown> {
+  return sequelize.transaction(async (transaction: Transaction) => {
+    const state = {
+      auth: {
+        user,
+        type: AuthenticationType.APP,
+        token: user.getJwtToken(),
+      },
+      transaction,
+    };
+    return fn({
+      state,
+      request: {
+        ip: faker.internet.ip(),
+      },
+    } as APIContext);
+  });
 }

vaderyang/baozi

commit 6c632d8edd494b87fad56a7bfbcae95d558d41d5

fix: notifications.pixel errors, regressed in 5780959e93d23568e7b83bd2472568e6895926ff

server/commands/notificationUpdater.ts
@@ -34,12 +34,19 @@ export default async function notificationUpdater(
   if (changed) {
     await notification.save({ transaction });
 
-    await Event.createFromContext(ctx, {
-      name: "notifications.update",
-      userId: notification.userId,
-      modelId: notification.id,
-      documentId: notification.documentId,
-    });
+    await Event.createFromContext(
+      ctx,
+      {
+        name: "notifications.update",
+        userId: notification.userId,
+        modelId: notification.id,
+        documentId: notification.documentId,
+      },
+      {
+        actorId: notification.userId,
+        teamId: notification.teamId,
+      }
+    );
   }
 
   return notification;

server/models/Event.ts
@@ -162,16 +162,19 @@ class Event extends IdModel<
   static createFromContext(
     ctx: APIContext,
     attributes: Omit<Partial<Event>, "ip" | "teamId" | "actorId"> = {},
+    defaultAttributes: Pick<Partial<Event>, "ip" | "teamId" | "actorId"> = {},
     options?: CreateOptions<InferAttributes<Event>>
   ) {
-    const { user } = ctx.state.auth;
+    const user = ctx.state.auth?.user;
+    const authType = ctx.state.auth?.type;
+
     return this.create(
       {
         ...attributes,
-        actorId: user.id,
-        teamId: user.teamId,
-        ip: ctx.request.ip,
-        authType: ctx.state.auth.type,
+        actorId: user?.id || defaultAttributes.actorId,
+        teamId: user?.teamId || defaultAttributes.teamId,
+        ip: ctx.request.ip || defaultAttributes.ip,
+        authType,
       },
       {
         transaction: ctx.state.transaction,