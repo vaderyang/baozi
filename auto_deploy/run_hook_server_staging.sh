715e56c441468685b011100f54378db670c1be37
franklin.zhi <franklin.zhi@netis.com>
2025-11-06 10:37:24 +0800

    [netis] Refine auto deploy function.
    [netis] Refine deploy scripts.

---
+++ auto_deploy/run_hook_server_staging.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+code_base_dir="$(dirname "$PWD")"
+image_version="staging"
+host_bind="0.0.0.0"
+port_bind="8082"
+echo "code_base_dir=$code_base_dir"
+echo "image_version=$image_version"
+echo "host_bind=$host_bind"
+echo "port_bind=$port_bind"
+
+#python -m auto_deploy.hook_server --code_base_dir "$code_base_dir" --image_version "$image_version" --host "$host_bind" --port "$port_bind"
+nohup python -m auto_deploy.hook_server --code_base_dir "$code_base_dir" --image_version "$image_version" --host "$host_bind" --port "$port_bind" > ./auto_deploy.staging.log 2>&1 &
+
