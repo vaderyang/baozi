0727d85a80afc93a3423c3e071ccb3fe39036ecb
Vader Yang <vader.yang@gmail.com>
2025-11-06 20:44:57 +0800

    fix: make MicrophoneIcon 30% smaller
    Apply 0.7 scale transform to make the icon more proportional

--- app/components/Icons/MicrophoneIcon.tsx
+++ app/components/Icons/MicrophoneIcon.tsx
@@ -19,15 +19,17 @@ const MicrophoneIcon: React.FC<Props> = ({
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
-    <path
-      d="M12 1C10.34 1 9 2.34 9 4V12C9 13.66 10.34 15 12 15C13.66 15 15 13.66 15 12V4C15 2.34 13.66 1 12 1Z"
-      fill={fill}
-    />
-    <path
-      d="M19 10V12C19 15.87 15.87 19 12 19C8.13 19 5 15.87 5 12V10H3V12C3 16.97 7.03 21 12 21C16.97 21 21 16.97 21 12V10H19Z"
-      fill={fill}
-    />
-    <path d="M11 21H13V24H11V21Z" fill={fill} />
+    <g transform="translate(12, 12) scale(0.7) translate(-12, -12)">
+      <path
+        d="M12 1C10.34 1 9 2.34 9 4V12C9 13.66 10.34 15 12 15C13.66 15 15 13.66 15 12V4C15 2.34 13.66 1 12 1Z"
+        fill={fill}
+      />
+      <path
+        d="M19 10V12C19 15.87 15.87 19 12 19C8.13 19 5 15.87 5 12V10H3V12C3 16.97 7.03 21 12 21C16.97 21 21 16.97 21 12V10H19Z"
+        fill={fill}
+      />
+      <path d="M11 21H13V24H11V21Z" fill={fill} />
+    </g>
   </svg>
 );
 
