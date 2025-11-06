282274f9104aecab5acad92a32351892b4afe9ec
franklin.zhi <franklin.zhi@netis.com>
2025-11-06 17:46:08 +0800

    [netis] Fix problems of scripts and samples.

--- auto_deploy/run_hook_server_staging.sh
+++ auto_deploy/run_hook_server_staging.sh
@@ -10,5 +10,5 @@ echo "host_bind=$host_bind"
 echo "port_bind=$port_bind"
 
 #python -m auto_deploy.hook_server --code_base_dir "$code_base_dir" --image_version "$image_version" --host "$host_bind" --port "$port_bind"
-nohup python -m auto_deploy.hook_server --code_base_dir "$code_base_dir" --image_version "$image_version" --host "$host_bind" --port "$port_bind" > ./auto_deploy.staging.log 2>&1 &
+nohup python -m hook_server --code_base_dir "$code_base_dir" --image_version "$image_version" --host "$host_bind" --port "$port_bind" > ./hook_server.log 2>&1 &
 
